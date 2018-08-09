0c3900efe8975705c7157aa3924cfb98b0b56315
黄官易 <hgy@x.xinhai.com>
2018-08-09 18:01:12 +0800

    新增 新增 调度日志 数据层接口

--- src/main/java/com/xh/dao/VertexRunningRegulateDao.java
+++ src/main/java/com/xh/dao/VertexRunningRegulateDao.java
@@ -40,6 +40,10 @@ public class VertexRunningRegulateDao {
 			sql.append(
 					" union select * from agv_vertex_running_regulate where start_vertex_code =? and end_vertex_code =? and vrr_status = 'process'");
 			psmt = conn.prepareStatement(sql.toString());
+			psmt.setString(1, startVertexCode);
+			psmt.setString(2, endVertexCode);
+			psmt.setString(3, endVertexCode);
+			psmt.setString(4, startVertexCode);
 			rs = psmt.executeQuery();
 			if (rs.next()) {
 				VertexRunningRegulate vrrObj = new VertexRunningRegulate();
@@ -73,11 +77,11 @@ public class VertexRunningRegulateDao {
 	 * @date 2018年8月8日
 	 * @version 1.0
 	 */
-	public int insertVRR(VertexRunningRegulate insertObj) throws Exception {
+	public String insertVRR(VertexRunningRegulate insertObj) throws Exception {
 		DruidPooledConnection conn = instance.getConnection();
 		PreparedStatement psmt = null;
 		try {
-			String sql = "insert into vertex_running_regulate(vrr_code,car_code,start_vertex_code,end_vertex_code,vrr_odo,vrr_start_time,vrr_end_time,vrr_status) values(?,?,?,?,?,?,?,?)";
+			String sql = "insert into agv_vertex_running_regulate(vrr_code,car_code,start_vertex_code,end_vertex_code,vrr_odo,vrr_start_time,vrr_end_time,vrr_status) values(?,?,?,?,?,?,?,?)";
 			psmt = conn.prepareStatement(sql);
 			psmt.setString(1, insertObj.getVrrCode());
 			psmt.setString(2, insertObj.getCarCode());
@@ -87,12 +91,42 @@ public class VertexRunningRegulateDao {
 			psmt.setDate(6, insertObj.getVrrStartTime());
 			psmt.setDate(7, insertObj.getVrrEndTime());
 			psmt.setString(8, insertObj.getVrrStatus());
+			return psmt.executeUpdate() > 0 ? insertObj.getVrrCode() : "";
+		} catch (SQLException e) {
+			log.error("添加调度信息数据库操作异常,异常原因:【" + e.toString() + "】");
+		} finally {
+			SqlPoolUtil.closeConnection(conn, psmt, null);
+		}
+		return "";
+	}
+
+	/**
+	 * 
+	 * @Title: updateVrrByVrrId
+	 * @Description: 更新调度日志
+	 * @author 黄官易
+	 * @param vrrCode
+	 * @return
+	 * @throws Exception
+	 * @return int
+	 * @date 2018年8月9日
+	 * @version 1.0
+	 */
+	public int updateVrrByVrrId(String vrrCode) throws Exception {
+		DruidPooledConnection conn = instance.getConnection();
+		String sql = "update agv_vertex_running_regulate set vrr_status = ? where vrr_code = ?";
+		PreparedStatement psmt = null;
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, "finish");
+			psmt.setString(2, vrrCode);
 			return psmt.executeUpdate();
 		} catch (SQLException e) {
-			log.error("");
+			log.error("更新调度日志数据库操作异常,异常原因:【" + e.toString() + "】");
 		} finally {
 			SqlPoolUtil.closeConnection(conn, psmt, null);
 		}
 		return 0;
 	}
+
 }
