929df93aa653ad31dbbc5076738a99ec4b4ff92d
黄官易 <hgy@x.xinhai.com>
2018-08-10 09:21:14 +0800

    修改 修改 当agv小车线路为1条时无限切换线路的异常

--- src/main/java/com/xh/controller/AGVSchedulingController.java
+++ src/main/java/com/xh/controller/AGVSchedulingController.java
@@ -1,6 +1,10 @@
 package com.xh.controller;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.xh.entity.Car;
 import com.xh.entity.Edge;
@@ -9,7 +13,7 @@ import com.xh.service.EdgeService;
 import com.xh.service.VertexRunningRegulateService;
 
 public class AGVSchedulingController extends Thread {
-
+	private static Logger log = LoggerFactory.getLogger(AGVSchedulingController.class);
 	private CarService carService = new CarService();
 	private EdgeService edgeService = new EdgeService();
 	private VertexRunningRegulateService vvrService = new VertexRunningRegulateService();
@@ -47,6 +51,7 @@ public class AGVSchedulingController extends Thread {
 		int currentArrIndex = 0; // 当前路线数组下标
 		if (null == runningPathArr || runningPathArr.length == 0) {
 			System.err.println("无路线，任务结束");
+			log.info("无路线，任务结束");
 			return;
 		}
 		// 初始化当前数组下标
@@ -61,21 +66,32 @@ public class AGVSchedulingController extends Thread {
 			if (vvrService.clockEdge(runningPathArr[i], runningPathArr[i + 1], car.getCarCode(), 0d)) {
 
 			} else {
-				while (currentIndex == car.getCurrentIndex()) {
+				while (currentIndex == car.getCurrentIndex() && car.getRouteScheme().size() > 1) {
 					car.initRunningStatus(currentVertexCode);// 切换线路
+					/*
+					 * System.err.println("当前线路被占用,进行线路切换。切换请求" + currentIndex +"---"+
+					 * car.getCurrentIndex() + (currentIndex == car.getCurrentIndex() &&
+					 * car.getRouteScheme().size() > 1));
+					 */
 				}
 				runingAgvCar(car);
 				break;
 			}
 			if (i == runningPathArr.length - 2) {
 				System.err.println("AGV车" + car.getCarName() + "任务完成");
+				log.info("AGV车" + car.getCarName() + "任务完成");
 			}
 		}
 
 	}
 
 	public static void main(String[] arg) {
-		for (int i = 0; i < 10; i++) {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("请输入模拟小车数(建议小车数量在20辆左右)：");
+		String carNumStr = scanner.nextLine();
+		int carNum = Integer.parseInt(carNumStr);
+		for (int i = 0; i < carNum; i++) {
 			new AGVSchedulingController().start();
 		}
 	}
